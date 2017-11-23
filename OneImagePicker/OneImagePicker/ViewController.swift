c6cc1dfa6ddd6ec8846eaf15b077d9ee6d15fa96
apbaca06 <31788893+apbaca06@users.noreply.github.com>
2017-11-23 13:46:13 +0800

    Add new project

---
+++ OneImagePicker/OneImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneImagePicker
+//
+//  Created by cindy on 2017/11/23.
+//  Copyright © 2017年 Jui-hsin.Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
